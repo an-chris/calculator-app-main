6e230de2f7a8ac80eea72f1e2f7f84e445de2350
Chris An <an-chris@outlook.com>
2021-08-26 00:00:43 -0500

    theme 2 switch
    - change input font weight
    - made some changes to theme 1

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const themeTwo = document.getElementById('theme-two');
 const themeThree = document.getElementById('theme-three');
 
 themeOne.addEventListener('click', function() {
-  // Theme switcher
+  document.body.style = 'background-color: hsl(222, 26%, 31%)'; // Body bg color
+  document.querySelector('.top').style = 'color: #fff'; // text color for top class
   document.querySelector('.top__right--box-circle').style = 'left: 3px;';
-  document.querySelector('.top__right--box-circle').style = 'background-color: hsl(6, 63%, 50%)';
-  document.querySelector('.top__right--box').style = 'background-color: hsl(223, 31%, 20%)';
+  document.querySelector('.top__right--box-circle').style = 'background-color: hsl(6, 63%, 50%)'; // switch circle
+  document.querySelector('.top__right--box').style = 'background-color: hsl(223, 31%, 20%)'; // switch background
+
   document.querySelector('.input').style = 'background-color: hsl(224, 36%, 15%); color: #fff'; // screen bg and text color
+
   document.querySelector('.keypad').style = 'background-color: hsl(223, 31%, 20%)'; // Keypad bg
   
   // Keypad buttons
@@ -24,10 +27,42 @@ themeOne.addEventListener('click', function() {
 
   // Equal button
   document.querySelector('.keypad__buttons--equals').style = 'background-color: hsl(6, 63%, 50%); box-shadow: 0 4px 0 0 hsl(6, 70%, 34%)';
+
+  // attribution 
+  document.querySelector('.attribution').style = 'color: #fff';
+
 });
 
 themeTwo.addEventListener('click', function() {
-  document.querySelector('.top__right--box-circle').style = 'left: 24px;';
+  // body
+  document.body.style = 'background-color: hsl(0, 0%, 90%);'; 
+
+  // top
+  document.querySelector('.top').style = 'color: hsl(60, 10%, 19%)';
+  document.querySelector('.top__right--box-circle').style = 'left: 24px; background-color: hsl(25, 98%, 40%)';
+  document.querySelector('.top__right--box').style = 'background-color: hsl(0, 5%, 81%)';
+
+  // input
+  document.querySelector('.input').style = 'background-color: hsl(0, 0%, 93%); color: hsl(60, 10%, 19%)';
+  
+  // keypad
+  document.querySelector('.keypad').style = 'background-color: hsl(0, 5%, 81%)';
+
+  // keypad buttons
+  const keypadButtons = document.querySelectorAll('.keypad__buttons');
+  for (let i = 0; i < keypadButtons.length; i++) {
+    keypadButtons[i].style.cssText = 'background-color: hsl(30, 25%, 89%); color: hsl(60, 10%, 19%); box-shadow: 0 4px 0 0 hsl(28, 16%, 65%);';
+  };
+
+  // delete and reset buttons
+  document.querySelector('.keypad__buttons--del').style = 'background-color: hsl(185, 42%, 37%); box-shadow: 0 4px 0 0 hsl(185, 58%, 25%)';
+  document.querySelector('.keypad__buttons--reset').style = 'background-color: hsl(185, 42%, 37%); box-shadow: 0 4px 0 0 hsl(185, 58%, 25%)';
+  
+  // equal button
+  document.querySelector('.keypad__buttons--equals').style = 'background-color: hsl(25, 98%, 40%); box-shadow: 0 4px 0 0 hsl(25, 99%, 27%)';
+
+  // attribution 
+  document.querySelector('.attribution').style = 'color: hsl(60, 10%, 19%)';
 });
 
 themeThree.addEventListener('click', function() {
